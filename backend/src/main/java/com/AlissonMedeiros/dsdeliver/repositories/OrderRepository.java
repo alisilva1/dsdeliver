d38b7517d5153206ad3bf464e65a0875fd350518
Alisson Medeiros <alisilva1@gmail.com>
2021-01-09 23:48:07 -0300

    GET /orders adjust

--- backend/src/main/java/com/AlissonMedeiros/dsdeliver/repositories/OrderRepository.java
+++ backend/src/main/java/com/AlissonMedeiros/dsdeliver/repositories/OrderRepository.java
@@ -1,9 +1,15 @@
 package com.AlissonMedeiros.dsdeliver.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.AlissonMedeiros.dsdeliver.entities.Order;
 
 public interface OrderRepository extends JpaRepository<Order, Long>  {
-
+	
+	@Query("SELECT DISTINCT obj from Order obj JOIN FETCH obj.products "
+			+ " WHERE obj.status = 0 ORDER BY obj.moment ASC")
+	List<Order> findOrdersWithProducts();
 }
--- backend/src/main/java/com/AlissonMedeiros/dsdeliver/services/OrderService.java
+++ backend/src/main/java/com/AlissonMedeiros/dsdeliver/services/OrderService.java
@@ -19,7 +19,7 @@ public class OrderService {
 		
 		@Transactional(readOnly = true)
 		public List<OrderDTO> findAll(){
-			List<Order> list = repository.findAll();
+			List<Order> list = repository.findOrdersWithProducts();
 				return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
 		}
 }
